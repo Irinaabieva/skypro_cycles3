fd3639a090f4df4789a07b9b608a351a07d9baa2
VarzievE <edvarziev@gmail.com>
2022-11-10 18:12:35 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 1
+        //Задание 2
         int save = 29000;
         int sum = 0;
         for (int i = 1; i <= 12; i++){
+            sum = sum + sum/100;
             sum = sum + save;
             System.out.println("Месяц " + i + " , сумма накоплений равна " + sum + " рублей");
         }
