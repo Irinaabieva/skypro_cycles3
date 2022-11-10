3cc084b8c834b0f1743622019b04bd1cfbc3f873
VarzievE <edvarziev@gmail.com>
2022-11-10 17:55:53 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        int save = 29000;
+        int sum = 0;
+        for (int i = 0; i <= 12; i++){
+            sum = sum + save;
+            System.out.println("Месяц " + i + " , сумма накоплений равна " + sum + " рублей");
+        }
     }
 }
